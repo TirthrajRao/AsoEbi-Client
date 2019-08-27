0cf9aa7ad4f3e4f29e82fdc1066edbb72737a007
tirthrajrao <tirthrajbarot2394@gmail.com>
2019-08-27 17:00:46 +0530

    responsive of reset password and forgot password

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -18,45 +18,49 @@ export class ResetPasswordComponent implements OnInit {
 
   ngOnInit() {
 
+    // menu toggle start
+    
+    // menu toggle end
+
     /**
      * Reset password form
      */
-    this.resetPasswordForm = new FormGroup({
-      currentPassword: new FormControl('', [Validators.required]),
-      newPassword: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
-      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)])
-    })
-  }
+     this.resetPasswordForm = new FormGroup({
+       currentPassword: new FormControl('', [Validators.required]),
+       newPassword: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
+       confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)])
+     })
+   }
 
   /**
    * Reset password functionality
    */
-  resetPassword() {
-    this.isDisable = true;
-    delete this.resetPasswordForm.value['confirmPassword']
-    console.log("current password value", this.resetPasswordForm.value);
-    this._loginService.resetPassword(this.resetPasswordForm.value)
-      .subscribe(data => {
-        console.log("reset password done by user", data);
-        this.isDisable = false;
-        this.router.navigate(['/home']);
-      }, (err: any) => {
-        console.log(err);
-        this.alertService.getError(err.message);
-      })
-  }
+   resetPassword() {
+     this.isDisable = true;
+     delete this.resetPasswordForm.value['confirmPassword']
+     console.log("current password value", this.resetPasswordForm.value);
+     this._loginService.resetPassword(this.resetPasswordForm.value)
+     .subscribe(data => {
+       console.log("reset password done by user", data);
+       this.isDisable = false;
+       this.router.navigate(['/home']);
+     }, (err: any) => {
+       console.log(err);
+       this.alertService.getError(err.message);
+     })
+   }
 
   /**
    * To show enterd newPassword and confirmPassowrd are same or not   
    */
-  comparePassword(form) {
-    console.log(form.value.newPassword == form.value.confirmPassword, this.match);
-    if (form.value.newPassword === form.value.confirmPassword) {
-      console.log("In true condition");
-      this.match = true;
-    } else {
-      this.match = false;
-    }
-  }
+   comparePassword(form) {
+     console.log(form.value.newPassword == form.value.confirmPassword, this.match);
+     if (form.value.newPassword === form.value.confirmPassword) {
+       console.log("In true condition");
+       this.match = true;
+     } else {
+       this.match = false;
+     }
+   }
 
-}
+ }
